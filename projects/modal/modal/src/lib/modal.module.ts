316dcd8a3f9817238f1aaf88d2cb0230a2b00433
Eduardo Feitosa <eduardofeitosa@Mac-mini-de-Eduardo.local>
2023-11-04 11:57:24 -0300

    modal lib -> remoção service desnecessário

--- projects/modal/modal/src/lib/modal.module.ts
+++ projects/modal/modal/src/lib/modal.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { ModalComponent } from './components/modal.component';
 
-
-
 @NgModule({
   declarations: [
     ModalComponent
--- projects/modal/modal/src/public-api.ts
+++ projects/modal/modal/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of modal
  */
 
-export * from './lib/services/modal.service';
 export * from './lib/components/modal.component';
 export * from './lib/modal.module';
